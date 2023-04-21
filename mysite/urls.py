a3e1c876c50b0018d7de7a0a3324f851df308b6f
Mitesh <mitesh>
2023-04-22 02:04:50 +0530

    route correction.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.shortcuts import HttpResponse
 
-def index_method(request):
+def owner(request):
     return HttpResponse("Hello, world. 25461651 is the polls owner.")
+
+def gotoQuestion(request):
+    return HttpResponse('<a href="">Answer to the Ultimate Question</a>')
 
 
 from django.contrib import admin
@@ -9,7 +12,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('owner/', index_method),
+    path('owner/', owner),
     path('polls/', include('polls.urls'))
 ]
 
