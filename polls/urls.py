50aebff0d32e1cb3d5dc27ca17661f5f6481d048
Mitesh <mitesh>
2023-04-25 23:08:32 +0530

    Solve error for form submit.

--- polls/urls.py
+++ polls/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from polls import views
 urlpatterns = [
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/results/', views.vote, name='results'),
 ]
